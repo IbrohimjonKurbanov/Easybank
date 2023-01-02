3d3ca4592b79e333715603b8510087b26a67a1cd
ibrohimjonkurbanov <ibrohimjonteamwork@gmail.com>
2023-01-03 00:55:47 +0500

    Easybank

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const headerToggleBtn =document.querySelector('.header__toggle-btn')
+const headerToggleBtnMenu =document.querySelector('#menu')
+const siteNav =document.querySelector('.sitenav')
+const overlay = document.querySelector('.overlay')
+
+headerToggleBtn.addEventListener('click', () => {
+  siteNav.classList.toggle('show-nav')
+  overlay.classList.toggle('show-nav')
+  
+  menu.classList.toggle('fa-times');
+})
+
+    
